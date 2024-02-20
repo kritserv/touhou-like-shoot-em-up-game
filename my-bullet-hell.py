e3afb2705e5ec31bc027848b08d60142583db2d4
kritserv <krit.serv@gmail.com>
2024-02-20 15:40:24 +0700

    add scrolling background

--- my-bullet-hell.py
+++ my-bullet-hell.py
@@ -1,16 +1,41 @@
 import pygame
 from pygame.locals import *
+from math import ceil
 
-screen_width = 600
-screen_height = 800
+pygame.init()
+
+clock = pygame.time.Clock()
+fps = 60
+
+screen_width = 1000
+screen_height = 1024
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Bullet Hell")
 
+bg = pygame.image.load("img/background.png").convert()
+bg_height = bg.get_height()
+
+scroll = 0
+tiles = ceil(screen_height / bg_height) + 1
+print(tiles)
+
 run = True
 while run:
+
+	clock.tick(fps)
+
+	for i in range(0, tiles):
+		screen.blit(bg, (0, i * bg_height + scroll))
+
+	scroll -= 6
+
+	if abs(scroll) > bg_height:
+		scroll = 0
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+	pygame.display.update()
 
 pygame.quit()
