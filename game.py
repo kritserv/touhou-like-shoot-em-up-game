53126465f489830b6cf3212289e3d1041199f795
kritserv <krit.serv@gmail.com>
2024-03-17 14:07:26 +0700

    handle all game state event in one for loop

--- game.py
+++ game.py
@@ -28,25 +28,16 @@ game_start = True
 run = True
 pause = False
 prev_time = time.time()
+
 while run:
 	clock.tick()
 	dt = time.time() - prev_time
 	prev_time = time.time()
-
 	if title_screen:
 		show_title_screen()
-					
-		for event in pygame.event.get():
-			run = check_quit_game_event(event)
-			title_screen_toggle = check_any_key_event(event)
-			if title_screen_toggle:
-				start_game()
-				title_screen = not title_screen
-			check_f_and_f11_key_event(event)
 
 	else:
 		if game_start:
-				
 			if bullet_hit_enemy():
 				enemy.take_damage(player.power)
 				player.score += 30
@@ -67,20 +58,6 @@ while run:
 					game_start = finish_game()
 			else:
 				update_graze_bullet()
-						
-			for event in pygame.event.get():
-				run = check_quit_game_event(event)
-				toggle_pause = check_esc_key_event(event)
-				if toggle_pause:
-					pause_game()
-					pause = not pause
-				quick_retry = check_r_key_event(event)
-				if quick_retry:
-					if pause:
-						pause = not pause
-					clear_all_bullet()
-					play_again()
-				check_f_and_f11_key_event(event)
 
 			if player.bombing:
 				bomb_enemy_and_bullet(pause, dt)
@@ -100,25 +77,41 @@ while run:
 			draw_ui_text(hi_score)
 			enemy.draw_health_bar()
 			player_group.draw(screen)
+			if player.show_hitbox: player.draw_hitbox()
 			player.draw_bomb()
 			playerbullet_group.draw(screen)
 			enemy_group.draw(screen)
 			enemybullet_group.draw(screen)
 
-			if player.show_hitbox:
-			    player.draw_hitbox()
-
 		else:
 			show_play_again()
 
-			for event in pygame.event.get():
-				run = check_quit_game_event(event)
-				retry = check_r_key_event(event)
-				if retry:
-					play_again()
-					hi_score = load_highscore()
-					game_start = True
-				check_f_and_f11_key_event(event)
+	for event in pygame.event.get():
+		run = check_quit_game_event(event)
+		check_f_and_f11_key_event(event)
+		if title_screen:
+			title_screen_toggle = check_any_key_event(event)
+			if title_screen_toggle:
+				start_game()
+				title_screen = not title_screen
+		if game_start:
+			toggle_pause = check_esc_key_event(event)
+			if toggle_pause:
+				pause_game()
+				pause = not pause
+			quick_retry = check_r_key_event(event)
+			if quick_retry:
+				if pause:
+					pause = not pause
+				clear_all_bullet()
+				play_again()
+		else:
+			retry = check_r_key_event(event)
+			if retry:
+				play_again()
+				hi_score = load_highscore()
+				game_start = True
+
 
 	pygame.display.flip()
 
